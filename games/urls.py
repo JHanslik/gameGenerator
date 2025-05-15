8dfd995f617634a7c283b539aa672de64c0341fe
JHanslik <julien.hanslik@gmail.com>
2025-05-15 14:17:12 +0200

    page favoris accessible

--- games/urls.py
+++ games/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('my-games/', views.my_games, name='my_games'),
     path('create/', views.game_create, name='game_create'),
     path('create-random/', views.random_game_create, name='random_game_create'),
+    path('favorites/', views.favorites, name='favorites'),
     path('<slug:slug>/', views.game_detail, name='game_detail'),
     path('<slug:slug>/favorite/', views.toggle_favorite, name='toggle_favorite'),
     path('<slug:slug>/toggle-public/', views.toggle_public, name='toggle_public'),
-    path('favorites/', views.favorites, name='favorites'),
 ]
